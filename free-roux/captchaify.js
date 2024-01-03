51116509a5a85bbc9a35b19a1327924a2363523f
cheddar-cheese1234 <chen.wang88@icloud.com>
2024-01-03 20:15:37 +0000

    Loop captcha

--- free-roux/captchaify.js
+++ free-roux/captchaify.js
@@ -4,3 +4,11 @@ const suspicious = () => {
     text.style.display = "block";
     captcha.style.display = "inline";
 }
+
+const loop = () => {
+    grecaptcha.render(document.getElementById('sus'), {
+        sitekey: '6LcY9kQpAAAAAFBsPVIK4tDINHrQmrQsbzudKbgm',
+        theme: 'dark',
+        callback: loop()
+    })
+}
