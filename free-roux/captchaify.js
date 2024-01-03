5a33dab4e9716341a47462abe96d24a55bca0bf2
cheddar-cheese1234 <chen.wang88@icloud.com>
2024-01-03 19:37:45 +0000

    New page; infinite captcha

---
+++ free-roux/captchaify.js
@@ -0,0 +1,6 @@
+const suspicious = () => {
+    let text = document.getElementById("suspicious");
+    let captcha = document.getElementById("sus");
+    text.style.display = "block";
+    captcha.style.display = "inline";
+}
